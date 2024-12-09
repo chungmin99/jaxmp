7dbce065629a813fd46aa1cb06c921232f395a32
Chung Min Kim <chungmin99@gmail.com>
2024-12-08 23:40:01 -0800

    Move coll-ik solve to solve_ik

--- src/jaxmp/extras/__init__.py
+++ src/jaxmp/extras/__init__.py
@@ -1,3 +1,4 @@
 from .urdf_loader import load_urdf as load_urdf
 from .solve_ik import solve_ik as solve_ik
+from .solve_ik import solve_ik_with_coll as solve_ik_with_coll
 from .grasp_antipodal import AntipodalGrasps as AntipodalGrasps
